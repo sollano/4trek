49ce3820e0e9aecd64e63ca6678b25975e6cb0a5
Sollano Rabelo Braga <sollanorb@gmail.com>
2017-12-15 09:42:31 -0200

    automated the code

--- processamento.R
+++ processamento.R
@@ -3,7 +3,7 @@
 library(ggplot2)
 source("CEP_regionalizado.r")
 
-dados_completo <- read.csv("dados/GPS.TXT")
+dados_completo <- read.csv("dados/GPS_OK_DATA.TXT")
 
 ## Preparar dados ####
 
@@ -12,7 +12,7 @@ str(dados_completo)
 head(dados_completo)
 
 ## Remover alguns dados
-dados_completo <- dados_completo[ ! dados_completo$Data %in% c("1/11/2017", "28/11/2017", "31/10/2017"),]
+#dados_completo <- dados_completo[ ! dados_completo$Data %in% c("1/11/2017", "28/11/2017", "31/10/2017"),]
 
 ## Limpar dados
 dados_completo <- data.cleaner(dados_completo)
@@ -24,8 +24,10 @@ head(dados_completo)
 ## Transformar dados em lista, baseado na data de coleta
 dados_lista <- split(dados_completo, dados_completo$Data)
 
-## Tamanho da lista
-length(dados_lista)
+## Determinar a ultima medicao, e a medicao teste
+medicao_teste <- length(dados_lista)
+ultima_med_cont <- length(dados_lista)-1
+
 ## Nomes dos objetos da lista
 names(dados_lista)
 
@@ -43,14 +45,14 @@ mp + geom_raster(aes(fill=media)) + scale_fill_continuous(na.value = 'white' )
 
 ## Calcular os limites com as primeiras 5 medições
 trans.limites <- limites.naturais(referencia = dados_lista[[1]], 
-                                  lista.arquivos = dados_lista[2:3], janela =  50 )
+                                  lista.arquivos = dados_lista[2:ultima_med_cont], janela =  50 )
 trans.limites 
 
 mp2 <- ggplot(data=trans.limites[["limites"]], aes(y=latitude, x=longitude))
 mp2 + geom_raster(aes(fill=LNS)) + scale_fill_continuous(na.value = 'white' )
 
 ## Testar os limites com as medições 6 e 7
-teste_1 <- verificar.dados(arquivo = dados_lista[[4]], limites = trans.limites)
+teste_1 <- verificar.dados(arquivo = dados_lista[[medicao_teste]], limites = trans.limites)
 teste_1
 
 ## Teste 1 grafico
