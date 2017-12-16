371c8d566b38e3dfafe50d081eef174a854f7192
Sollano R Braga <sollanorb@gmail.com>
2017-12-15 22:21:09 -0200

    add data

--- testando_medicoes.R
+++ testando_medicoes.R
@@ -1,10 +1,13 @@
 ## Preparar dados ####
 source("CEP_regionalizado.r")
 
-#dados_completo <- read.csv("dados/GPS.TXT")
-#dados_completo <- read.csv("dados/GPS_fixed.TXT")
-dados_completo <- read.csv("dados/GPS_OK_DATA.TXT")
+#dados_completo <- read.csv("dados/GPS_old.TXT")
+dados_completo <- read.csv("dados/GPS.TXT")
+#dados_completo <- read.csv("dados/GPS_fixit.TXT")
+#dados_completo <- read.csv("dados/GPS_13-12.TXT")
+#dados_completo <- read.csv("dados/GPS_OK_DATA.TXT")
 
 #dados_completo <- dados_completo[ ! dados_completo$Data %in% c("1/11/2017", "28/11/2017", "31/10/2017", "6/11/2017"),]
-
+levels(dados_completo$Data)
+#compara_medicoes(dados_completo[dados_completo$Data=="15/12/2017",], 50)
 compara_medicoes(dados_completo, 50)
