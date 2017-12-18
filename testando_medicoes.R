6f7cb08a00dbf22f621e86d1ae9b30c5443bcaf5
Sollano Rabelo Braga <sollanorb@gmail.com>
2017-12-18 21:33:51 -0200

    new data

--- testando_medicoes.R
+++ testando_medicoes.R
@@ -1,9 +1,9 @@
 ## Preparar dados ####
 source("CEP_regionalizado.r")
 
-#dados_completo <- read.csv("dados/GPS.TXT")
+dados_completo <- read.csv("dados/GPS.TXT")
 #dados_completo <- read.csv("dados/GPS_fixed.TXT")
-dados_completo <- read.csv("dados/GPS_OK_DATA.TXT")
+#dados_completo <- read.csv("dados/GPS_OK_DATA.TXT")
 
 #dados_completo <- dados_completo[ ! dados_completo$Data %in% c("1/11/2017", "28/11/2017", "31/10/2017", "6/11/2017"),]
 
