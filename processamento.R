048b469b46e92433ef1b247b2e27a22b214225cf
Sollano Rabelo Braga <sollanorb@gmail.com>
2017-12-19 00:17:28 -0200

    better plots

--- processamento.R
+++ processamento.R
@@ -38,8 +38,27 @@ trans.rep0 <- media.regionalizada(dados_lista[[1]], janela = 50 )
 trans.rep0
 
  ## Plotar a média regionalizada
-mp <- ggplot(data=trans.rep0[[ 2 ]], aes(y=latitude, x=longitude))
-mp + geom_raster(aes(fill=media)) + scale_fill_continuous(na.value = 'white' )
+mp <- ggplot(data=trans.rep0[[ 2 ]], aes(y=latitude, x=longitude)) + 
+  geom_raster(aes(fill=media)) + 
+  scale_fill_continuous(na.value = 'white' ) +
+  labs(x="Longitude", y="Latitude", fill="Velocidade média (km/h)") +
+  theme(
+    legend.position = "bottom",
+    legend.text = element_text(size = 20), 
+    legend.title = element_text(size = 20),
+    legend.key.width = unit( 2, units = "cm"), 
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    axis.title   = element_text(size = 26,face="bold"), 
+    axis.text    = element_text(size = 22),
+    axis.line.x = element_line(color="black"),
+    axis.line.y = element_line(color="black"),
+    strip.text.x = element_text(size = 22)   )
+
+mp
+
+ggsave("plots/media_reg_medicao_1.jpg", plot = mp, width = 12, height = 10)
 
 ## Teste da função limites.naturais ####
 
@@ -48,17 +67,75 @@ trans.limites <- limites.naturais(referencia = dados_lista[[1]],
                                   lista.arquivos = dados_lista[2:ultima_med_cont], janela =  50 )
 trans.limites 
 
-mp2 <- ggplot(data=trans.limites[["limites"]], aes(y=latitude, x=longitude))
-mp2 + geom_raster(aes(fill=LNS)) + scale_fill_continuous(na.value = 'white' )
+mp2 <- ggplot(data=trans.limites[["limites"]], aes(y=latitude, x=longitude)) + 
+  geom_raster(aes(fill=LNS)) +
+  scale_fill_continuous(na.value = 'white' ) +
+  labs(x="Longitude", y="Latitude", fill="Limite natural superior") +
+  theme(
+    legend.position = "bottom",
+    legend.text = element_text(size = 20), 
+    legend.title = element_text(size = 20),
+    legend.key.width = unit( 2, units = "cm"), 
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    axis.title   = element_text(size = 26,face="bold"), 
+    axis.text    = element_text(size = 22),
+    axis.line.x = element_line(color="black"),
+    axis.line.y = element_line(color="black"),
+    strip.text.x = element_text(size = 22)   )
+mp2
 
-## Testar os limites com as medições 6 e 7
+ggsave("plots/media_reg_all_lns.jpg", plot = mp2, width = 12, height = 10)
+
+mp3 <- ggplot(data=trans.limites[["limites"]], aes(y=latitude, x=longitude)) + 
+  geom_raster(aes(fill=LNI)) +
+  scale_fill_continuous(na.value = 'white' ) +
+  labs(x="Longitude", y="Latitude", fill="Limite natural inferior") +
+  theme(
+    legend.position = "bottom",
+    legend.text = element_text(size = 20), 
+    legend.title = element_text(size = 20),
+    legend.key.width = unit( 2, units = "cm"), 
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    axis.title   = element_text(size = 26,face="bold"), 
+    axis.text    = element_text(size = 22),
+    axis.line.x = element_line(color="black"),
+    axis.line.y = element_line(color="black"),
+    strip.text.x = element_text(size = 22)   )
+mp3
+
+ggsave("plots/media_reg_all_lns.jpg", plot = mp3, width = 12, height = 10)
+
+
+## Testar os limites com a ultima medição
 teste_1 <- verificar.dados(arquivo = dados_lista[[medicao_teste]], limites = trans.limites)
 teste_1
 
 ## Teste 1 grafico
-mp <- ggplot(data=na.omit(teste_1), aes(y=latitude, x=longitude))
-mp + geom_raster(aes(fill=check)) + theme_bw()
+mp_teste <- ggplot(data=na.omit(teste_1), aes(y=latitude, x=longitude)) +
+  geom_raster(aes(fill=check)) + 
+  theme_bw() +
+  labs(x="Longitude", y="Latitude") +
+  theme(
+    legend.position = "bottom",
+    legend.text = element_text(size = 20), 
+    legend.title = element_text(size = 20),
+    legend.key.width = unit( 2, units = "cm"), 
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    axis.title   = element_text(size = 26,face="bold"), 
+    axis.text    = element_text(size = 22),
+    axis.line.x = element_line(color="black"),
+    axis.line.y = element_line(color="black"),
+    strip.text.x = element_text(size = 22)   )
 
+mp_teste
+
+ggsave("plots/media_reg_all_lns.jpg", plot = mp_teste, width = 12, height = 10)
 
 
 
