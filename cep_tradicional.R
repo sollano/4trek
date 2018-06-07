aad962ac44b728341bd31841e652b927e8569b50
sollano <sollanorb@gmail.com>
2018-06-07 16:54:46 -0300

    fix cep

--- cep_tradicional.R
+++ cep_tradicional.R
@@ -1,9 +1,10 @@
 library(qcc)
+library(tidyverse)
 
 dados_completo <- data.cleaner( read.csv("dados/GPS_FINAL_DATA.TXT") )
 
 vel <- 
-dados_completo %>% 
+  dados_completo %>% 
   select(Velocidade, Data) %>%
   group_by(Data) %>% 
   mutate(id=row_number()) %>% 
@@ -14,5 +15,7 @@ vel
 tail(vel)
 dim(vel)
 
-q_cep_trad <- qcc(teste[1:927,], type="xbar", newdata = teste[928:1855,] )
+q_cep_trad <- qcc(vel[1:927,], type="xbar", newdata = vel[928:1855,] )
 plot(q_cep_trad, chart.all=FALSE)
+
+
